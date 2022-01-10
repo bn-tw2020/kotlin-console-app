19b5dd9526f74bfd6b6b444737e678a066a3e945
bn-tw2020 <skaxodn97@gmail.com>
2022-01-10 20:17:36 +0900

    feat: 인사말, 사용자 입력 정보 출력

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,16 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    /*
+    Step1. 인사말 출력
+    Step2. 사용자 일므 받기
+    Step3. 입력 받은 사용자 이름 출력
+     */
+    println("안녕하세요, Shoppi 에 오신 것을 환영합니다!")
+    println("쇼핑을 계속 하시려면 이름을 입력해주세요 :)")
+
+    val name = readLine()
+    println("""
+        감사합니다. 반갑습니다, $name 님
+        원하시는 카테고리를 입력해주세요.
+        ***====================================***
+    """.trimIndent())
 }
